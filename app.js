8b6810d1a98dc7fb4e3613ad05b024cba50d075f
josiahnissi <josiah@appinessworld.com>
2020-01-06 13:55:36 +0530

    1. Added gitignore file 2. npm init 3. Added npm-validator package

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const validator = require('validator')
 const notesContent = require('./notes')
 
 const notes = notesContent()
 
 console.log(notes)
+
+console.log(validator.isEmail('nissi@example.com'))
